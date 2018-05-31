7c6c414c9cd3b95c8d12dc24e9aa5ecdb3fc98d2
Lijie Huang <huanglijie@outlook.com>
2018-05-31 15:23:17 +0800

    estimate voxel-specific HRF

--- emocode/glmdenoise/glmdenoise_pipeline.m
+++ emocode/glmdenoise/glmdenoise_pipeline.m
@@ -1,4 +1,4 @@
-function [design, data, results, denoiseddata] = glmdenoise_pipeline(subj, session)
+function [design, data, s1results, hrfs, betas, R2] = glmdenoise_pipeline(subj, session)
 % 
 % GLMDenoise pipeline script
 % [results, denoiseddata] = glmdenoise(subj, session)
@@ -24,8 +24,68 @@ for i=1:length(run_list)
     data{i} = nii.img;
 end
 
+% experiment parameter config
 stimdur = 2;
 tr = 2;
-[results, denoiseddata] = GLMdenoisedata(design, data, stimdur, tr, [], [], [], 'figures');
+
+% A 2-stage analyse pipeine for extimate voxel-specific HRF
+% Step 1: Initial call to GLMdenoise to learn the noise regressors
+% Compute a canonical HRF.
+hrf = getcanonicalhrf(stimdur, tr)';
+% Make the initial call to GLMdenoise.  We indicate that the canonical HRF is to be 
+% assumed.  We also turn off bootstrapping as it is unnecessary here.
+s1results = GLMdenoisedata(design, data, stimdur, tr, ...
+            'assume', hrf, struct('numboots',0), 'stage1figures');
+
+% Extract the noise regressors based on the results.  Note that results.pcnum is 
+% the number of noise regressors that was selected by GLMdenoisedata.m.
+noisereg = cellfun(@(x) x(:,1:s1results.pcnum), s1results.pcregressors,'UniformOutput',0);
+% Inspect the dimensionality of the noise regressors.
+noisereg
+
+% Step 2: Re-analyze the data, tailoring the HRF voxel-by-voxel
+% Define some useful constants.
+xyzsize = [64 64 33];  % the XYZ dimensions of the dataset
+numcond = 4;           % the number of conditions in this dataset
+
+% Define an options struct.  This specifies (1) that the noise regressors determined
+% above will serve as extra regressors in the GLM, (2) that the HRF estimated 
+% from the data should always be used (even if it is very unlike the initial seed),
+% and (3) that we want to suppress text output (since many voxels will be analyzed
+% in a loop).
+opt = struct('extraregressors', {noisereg}, 'hrfthresh', -Inf, ...
+             'suppressoutput', 1);
+
+% Initialize the results.
+hrfs = zeros([xyzsize length(hrf)], 'single');
+betas = zeros([xyzsize numcond], 'single');
+R2 = zeros([xyzsize], 'single');
+
+% Loop over voxels.  Note: The following loop may take a long time to execute.
+% This loop can be speeded up using parfor or cluster-computing solutions, but
+% here we use a simple for-loop for the purpose of simplicity.
+cache = [];
+for zz=1:xyzsize(3)
+    fprintf('\nslice %d #', zz);
+    for xx=1:xyzsize(1)
+        fprintf('.');
+        for yy=1:xyzsize(2)
+            % Extract the time-series data for one voxel.
+            data0 = cellfun(@(x) flatten(x(xx,yy,zz,:)), data, 'UniformOutput', 0);
+
+            % Analyze the time-series, specifying that we want to optimize the HRF.
+            % We use the canonical HRF as the initial seed for the HRF.
+            [results0, cache] = GLMestimatemodel(design, data0, stimdur, tr, ...
+                                                'optimize', hrf, 0, opt, cache);
+
+            % Record the results.
+            hrfs(xx, yy, zz, :) = results0.modelmd{1};
+            betas(xx, yy, zz, :) = results0.modelmd{2};
+            R2(xx, yy, zz) = results0.R2;
+        end
+    end
 end
-    
+
+%[results, denoiseddata] = GLMdenoisedata(design, data, stimdur, tr, [], [], [], 'figures');
+end
+
